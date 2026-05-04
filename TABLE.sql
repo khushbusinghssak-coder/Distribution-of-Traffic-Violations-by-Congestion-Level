4cf02f05927801633561b43c7f3c6df05bcf276d
khushbusinghssak-coder <khushbusinghssak@gmail.com>
2026-05-04 16:32:34 +0530

    Add files via upload

---
+++ TABLE.sql
@@ -0,0 +1,10 @@
+CREATE TABLE TRAFFIC(
+days INT,
+hours INT,	
+vehicle_count	INT,
+average_speed	DECIMAL(6,2),
+congestion_level	VARCHAR(10),
+weather_condition	VARCHAR(20),
+traffic_rule_violation VARCHAR(50)
+);
+
